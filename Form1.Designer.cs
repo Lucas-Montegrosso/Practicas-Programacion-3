11df42400e1b8868ad53d2f030639d70e461f69e
Lucas-Montegrosso <149318204+Lucas-Montegrosso@users.noreply.github.com>
2024-08-30 20:47:40 +0000

    Primeras practicas C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lwElementos = new System.Windows.Forms.ListBox();
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbFecha = new System.Windows.Forms.Label();
+            this.dtpFechaDeNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.cBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(147, 439);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(192, 52);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lwElementos
+            // 
+            this.lwElementos.ItemHeight = 20;
+            this.lwElementos.Location = new System.Drawing.Point(129, 223);
+            this.lwElementos.Name = "lwElementos";
+            this.lwElementos.Size = new System.Drawing.Size(414, 204);
+            this.lwElementos.TabIndex = 1;
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(260, 108);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(165, 26);
+            this.txtElemento.TabIndex = 2;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(155, 111);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(73, 20);
+            this.lbNombre.TabIndex = 3;
+            this.lbNombre.Text = "Nombre: ";
+            // 
+            // lbFecha
+            // 
+            this.lbFecha.AutoSize = true;
+            this.lbFecha.Location = new System.Drawing.Point(63, 143);
+            this.lbFecha.Name = "lbFecha";
+            this.lbFecha.Size = new System.Drawing.Size(165, 20);
+            this.lbFecha.TabIndex = 4;
+            this.lbFecha.Text = "Fecha de nacimiento: ";
+            // 
+            // dtpFechaDeNacimiento
+            // 
+            this.dtpFechaDeNacimiento.Location = new System.Drawing.Point(260, 143);
+            this.dtpFechaDeNacimiento.Name = "dtpFechaDeNacimiento";
+            this.dtpFechaDeNacimiento.Size = new System.Drawing.Size(200, 26);
+            this.dtpFechaDeNacimiento.TabIndex = 5;
+            // 
+            // cBox
+            // 
+            this.cBox.AutoSize = true;
+            this.cBox.Location = new System.Drawing.Point(260, 193);
+            this.cBox.Name = "cBox";
+            this.cBox.Size = new System.Drawing.Size(107, 24);
+            this.cBox.TabIndex = 6;
+            this.cBox.Text = "Chocolate";
+            this.cBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(661, 566);
+            this.Controls.Add(this.cBox);
+            this.Controls.Add(this.dtpFechaDeNacimiento);
+            this.Controls.Add(this.lbFecha);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.lwElementos);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lwElementos;
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbFecha;
+        private System.Windows.Forms.DateTimePicker dtpFechaDeNacimiento;
+        private System.Windows.Forms.CheckBox cBox;
+    }
+}
+
